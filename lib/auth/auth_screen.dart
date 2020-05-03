d3f4b61a153a6db7e5aa520abe11ac3f896571e9
Jon <jon@Jons-Mac-mini.local>
2020-05-03 12:30:35 +0200

    auth screen redesign

--- lib/auth/auth_screen.dart
+++ lib/auth/auth_screen.dart
@@ -1,41 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gtd/core/core_blocs/navigator_bloc.dart';
-import 'package:gtd/core/styles.dart';
 import 'package:lottie/lottie.dart';
 
 class AuthScreen extends StatelessWidget {
-  Widget _createAuthMainScreen(BuildContext context, {LottieBuilder lottie, String title, Widget loginButton, Widget registerButton}) {
+  Widget _createAuthMainScreen(BuildContext context,
+      {LottieBuilder lottie, Widget loginButton, Widget registerButton}) {
+    Size size = MediaQuery.of(context).size;
+
     return Container(
       child: Padding(
         padding: EdgeInsets.all(40.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 60.0),
+            SizedBox(height: size.height / 12),
+            Text(
+              'Do Things!',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.left,
+            ),
+            Text(
+              'Empieza a trabajar con GTD hoy mismo.',
+              style: TextStyle(fontSize: 14),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(height: size.height / 12),
             Center(child: lottie),
-            SizedBox(height: 60.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  title,
-                  style: kTitleStyle,
-                  textAlign: TextAlign.center,
-                ),
-              ],
+            SizedBox(height: size.height / 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  loginButton,
+                  SizedBox(height: 15),
+                  registerButton
+                ],
+              ),
             ),
-            SizedBox(height: 60.0),
-            SizedBox(
-              width: double.infinity - 100,
-              height: MediaQuery.of(context).size.height / 12,
-              child: loginButton,
-            ),
-            SizedBox(height: 15.0),
-            SizedBox(
-              width: double.infinity - 100,
-              height: MediaQuery.of(context).size.height / 12,
-              child: registerButton,
+            Container(
+              width: size.width - 100,
+              child: Text(
+                'Utilizando el servicio de Do Things aceptas los términos y condiciones del servicio.',
+                style: TextStyle(fontSize: 12),
+                textAlign: TextAlign.center,
+              ),
             ),
           ],
         ),
@@ -45,44 +56,41 @@ class AuthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Widget loginButton = RaisedButton(
-      color: Colors.white,
+    final Widget loginButton = ButtonTheme(
+      minWidth: MediaQuery.of(context).size.width - 100,
+        child: RaisedButton(
+      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+      color: Colors.orange,
       onPressed: () => {
-        BlocProvider.of<NavigatorBloc>(context)
-            .add(NavigateToLoginEvent()),
+        BlocProvider.of<NavigatorBloc>(context).add(NavigateToLoginEvent()),
       },
-      child: Text('Iniciar Sesión', style: kButtonLabel),
-    );
+      child: Text('Iniciar Sesión',
+          style: TextStyle(color: Colors.white, fontSize: 18)),
+    ));
 
-    final Widget registerButton = RaisedButton(
-      color: Colors.white,
+    final Widget registerButton = ButtonTheme(
+      minWidth: MediaQuery.of(context).size.width - 100,
+        child: RaisedButton(
+      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+      color: Colors.orange,
       onPressed: () => {
-                BlocProvider.of<NavigatorBloc>(context)
-            .add(NavigateToRegisterEvent()),
+        BlocProvider.of<NavigatorBloc>(context).add(NavigateToRegisterEvent()),
       },
-      child: Text('Registrarse', style: kButtonLabel),
-    );
+      child: Text('Crear cuenta',
+          style: TextStyle(color: Colors.white, fontSize: 18)),
+    ));
 
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.blue[300],
+      backgroundColor: Colors.orange[200],
+      body: _createAuthMainScreen(
+        context,
+        lottie: Lottie.asset(
+          'assets/rocket.json',
+          width: MediaQuery.of(context).size.width / 2,
+          height: MediaQuery.of(context).size.width / 2,
         ),
-        child: ListView(
-          children: <Widget>[
-            _createAuthMainScreen(
-              context,
-              title: 'Empecemos!',
-              lottie: Lottie.asset(
-                'assets/rocket.json',
-                width: MediaQuery.of(context).size.width / 2,
-                height: MediaQuery.of(context).size.width / 2,
-              ),
-              loginButton: loginButton, 
-              registerButton: registerButton,
-            )
-          ],
-        ),
+        loginButton: loginButton,
+        registerButton: registerButton,
       ),
     );
   }
